e40bd76ff657240e1963439264bd50e961aa2e69
Marc Troll <marc.troll@nodelab-it.de>
2021-11-23 14:59:33 +0100

    changed clickaway from js to ts

--- plugins/clickaway.client.ts
+++ plugins/clickaway.client.ts
@@ -1,8 +1,9 @@
 import { defineNuxtPlugin } from "#app";
+import {DirectiveBinding, VNode} from "@vue/runtime-core";
 
 export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.vueApp.directive("clickaway", {
-        beforeMount(el, binding, vnode) {
+        beforeMount(el: any, binding: DirectiveBinding<any>, vnode: VNode<any, any>) {
             el.clickOutsideEvent = (event) => {
                 if(!(el === event.target || el.contains(event.target))) {
                     binding.value(el, event);
