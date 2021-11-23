124ef07ef9cf1d64efa9eb5c73a6d4f1e7b4419b
Marc Troll <marc.troll@nodelab-it.de>
2021-11-23 22:19:24 +0100

    reduced cookie max age and added cookie update to session management

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -4,9 +4,6 @@ import {BookingLib} from "~/server/lib/BookingLib";
 import {PoolConnection, RowDataPacket} from "mysql2/promise";
 
 export default async (req: IncomingMessage, res: ServerResponse) => {
-	// TODO. See session.ts
-	// @ts-ignore
-	console.log(req.sessionID);
 	/*const connection : PoolConnection = await BookingLib.getDatabase().getConnection();
 	const rows = await connection.execute<RowDataPacket[]>("SELECT * FROM test", []);
 	await connection.release();
--- server/middleware/session.ts
+++ server/middleware/session.ts
@@ -7,13 +7,22 @@ export default async (req : IncomingMessage, res: ServerResponse) => {
     const options : CookieSerializeOptions = {
         httpOnly: true,
         sameSite: "strict",
-        maxAge: 86400 * 180 // 180 Tage
+        maxAge: 86400 * 90 // 90 Tage
     };
     
-    if(!useCookie(req, "sessionID")) {
+    const cookieValue = useCookie(req, "sessionID");
+    
+    if(!cookieValue) {
+        // set cookie
         setCookie(res, "sessionID", randomBytes(20).toString('base64'), options);
+    } else {
+        // update cookie to prolong expiry date
+        if(!req.url.startsWith("/api/")) {
+            setCookie(res, "sessionID", cookieValue, options);
+        }
     }
     
+    // TODO
     // Doesn't work for now. See https://github.com/nuxt/framework/issues/1042
     // @ts-ignore
     req.sessionID = useCookie(req, "sessionID");
