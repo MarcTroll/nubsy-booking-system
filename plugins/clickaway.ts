75db363d491953986a9d0aa8b4bc2a70de9d44f5
Marc Troll <marc.troll@nodelab-it.de>
2022-02-18 18:37:05 +0100

    fixed clickaway directive to work with SSR

--- plugins/clickaway.ts
+++ plugins/clickaway.ts
@@ -3,16 +3,16 @@ import {DirectiveBinding, VNode} from "@vue/runtime-core";
 
 export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.vueApp.directive("clickaway", {
-        beforeMount(el: any, binding: DirectiveBinding<any>, vnode: VNode<any, any>) {
+        mounted(el: any, binding: DirectiveBinding<any>, vnode: VNode<any, any>) {
             el.clickOutsideEvent = (event) => {
                 if(!(el === event.target || el.contains(event.target))) {
                     binding.value(el, event);
                 }
             };
-            document.body.addEventListener("click", el.clickOutsideEvent);
+            document.addEventListener("click", el.clickOutsideEvent);
         },
         unmounted(el) {
-            document.body.removeEventListener("click", el.clickOutsideEvent);
+            document.removeEventListener("click", el.clickOutsideEvent);
         }
     });
 });
