20ce9f9fc78a8cbb7a8b9ff4a71de91ead0648ea
Marc Troll <marc.troll@nodelab-it.de>
2022-02-23 15:22:23 +0100

    add jsdocs to IMaxLengthFormField and IMinLengthFormField

--- server/lib/form/field/IMaxLengthFormField.ts
+++ server/lib/form/field/IMaxLengthFormField.ts
@@ -1,12 +1,34 @@
 /**
- * Adds form field support for setting maximum length of a text input.
+ * Adds form field support for setting maximum length of a text form field.
  * */
 export interface IMaxLengthFormField {
     
+    /**
+     * storing the maximum length a text form field may have. May be null.
+     * */
     maxLength: number;
     
+    /**
+     * Returns the maximum length for the form field.
+     *
+     * @returns {number} maximum length if set. Otherwise null
+     * */
     getMaxLength();
+    
+    /**
+     * Sets the maximum length for the form field.
+     *
+     * @param length {number} new maximum length of the form field
+     * @returns instance of the form field
+     * */
     setMaxLength(length : number);
-    validateMaxLength();
+    
+    /**
+     * Validates the maximum length for the form field.
+     *
+     * @returns {boolean} true if text length of form field is shorter than the provided maximum length or the maximum
+     * length is not set. false otherwise.
+     * */
+    validateMaxLength() : boolean;
     
 }
--- server/lib/form/field/IMinLengthFormField.ts
+++ server/lib/form/field/IMinLengthFormField.ts
@@ -1,12 +1,34 @@
 /**
- * Adds form field support for setting minimum length of a text input.
+ * Adds form field support for setting minimum length of a text form field.
  * */
 export interface IMinLengthFormField {
     
+    /**
+     * storing the minimum length a text form field must have. May be null.
+     * */
     minLength: number;
     
+    /**
+     * Returns the minimum length for the form field.
+     *
+     * @returns {number} minimum length if set. Otherwise null
+     * */
     getMinLength();
+    
+    /**
+     * Sets the minimum length for the form field.
+     *
+     * @param length {number} new maximum length of the form field
+     * @returns instance of the form field
+     * */
     setMinLength(length : number);
-    validateMinLength();
+    
+    /**
+     * Validates the minimum length for the form field.
+     *
+     * @returns {boolean} true if text length of form field is longer than the provided minimum length or the minimum
+     * length is not set. false otherwise.
+     * */
+    validateMinLength() : boolean;
     
 }
