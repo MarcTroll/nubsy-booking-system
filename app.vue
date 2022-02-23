7fb930ffe85646d11a245a8df2c42052a8230d43
Marc Troll <marc.troll@nodelab-it.de>
2022-02-23 17:03:15 +0100

    add rerouting to account management if no account exists

--- app.vue
+++ app.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
 import Header from "~/components/Header.vue";
 
-useTitle("nubsy - loading...")
-
 const authentication = useAuth();
+const route = useRoute();
+const router = useRouter();
 
 const res = await $fetch("/api/user/relogin", {
     method: "POST",
@@ -14,8 +14,25 @@ if(res.status === "success") {
     authentication.value.loggedIn = true;
     authentication.value.user = res.user;
 
-    // TODO: Check if user account exists
+    if(!authentication.value.user.name) {
+        if(!route.path.startsWith("/me/accounts")) {
+            await router.replace({
+                path: "/me/accounts"
+            });
+        }
+    }
 }
+
+watch(route, async (newRoute) => {
+    if(authentication.value.loggedIn && !authentication.value.user.name) {
+        if(!newRoute.path.startsWith("/me/accounts")) {
+            await router.replace({
+                path: "/me/accounts"
+            });
+        }
+    }
+})
+
 authentication.value.ready = true;
 </script>
 
