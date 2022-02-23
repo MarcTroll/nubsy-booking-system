3d54be30aa68a7ea7e6e4bdd930d4982cb63ca11
Marc Troll <marc.troll@nodelab-it.de>
2022-02-23 17:03:47 +0100

    moved file "me.vue" to "me/index.vue"

---
+++ pages/me/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+    useTitle("Anmelden");
+
+    const authentication = useAuth();
+</script>
+
+<template>
+    <div>
+        <div class="content">
+            <h2>
+                Accounts
+            </h2>
+        </div>
+    </div>
+</template>
