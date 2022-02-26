d704524021f6833af9f93ceffacd2e3ed0ed7a6b
Marc Troll <marc.troll@nodelab-it.de>
2022-02-27 00:13:10 +0100

    add PasswordFormField implementation to frontend

--- components/nubsy/form/field/NubsyPasswordFormField.vue
+++ components/nubsy/form/field/NubsyPasswordFormField.vue
@@ -1,5 +1,52 @@
+<script lang="ts" setup>
+interface PasswordFormField {
+    id: string;
+    type: string;
+    label: string | null;
+    value: string | null;
+    error: string;
+    minLength: number;
+}
+
+const props = defineProps<{
+    field: PasswordFormField
+}>();
+const formFieldData = reactive(props.field);
+const inputWarning = ref("");
+
+// TODO: Change that when @nuxt-community/i18n is available for nuxt 3
+function translateError(error) {
+    console.log(error)
+    switch(error) {
+        case "ERR_FORM_VALIDATION_VALUE_UNDEFINED":
+            return "Diese Feld muss ausgefüllt werden.";
+        case "ERR_FORM_VALIDATION_PASSWORD_TOO_SHORT":
+            return "Deine Eingabe muss mindestens " + formFieldData.minLength + " Zeichen lang sein.";
+        default:
+            return "Ein unbekannter Fehler ist aufgetreten.";
+    }
+}
+
+function validate() {
+    // Reset form field error to show warnings
+    formFieldData.error = "";
+    inputWarning.value = "";
+
+    if(!formFieldData || formFieldData.value.length === 0) {
+        inputWarning.value = "ERR_FORM_VALIDATION_VALUE_UNDEFINED";
+        return;
+    }
+    if(formFieldData.minLength && formFieldData.value.length < formFieldData.minLength) {
+        inputWarning.value = "ERR_FORM_VALIDATION_PASSWORD_TOO_SHORT";
+    }
+}
+</script>
+
 <template>
-    <div>
-        Passwortfeld
+    <div :class="{formError: formFieldData.error}" :id="formFieldData.id + 'Field'">
+        <label :for="formFieldData.id" v-if="formFieldData.label">{{formFieldData.label}}</label>
+        <input type="password" :id="formFieldData.id" v-model="formFieldData.value" @blur="validate()">
+        <div class="formWarning" v-if="!formFieldData.error && inputWarning">{{translateError(inputWarning)}}</div>
+        <div class="formErrorDescription" v-if="formFieldData.error">{{translateError(formFieldData.error)}}</div>
     </div>
 </template>
